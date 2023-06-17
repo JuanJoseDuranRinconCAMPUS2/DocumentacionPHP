9315435488d95e0079121ea81d66d0deb23c09a4
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-06-17 09:42:36 -0500

    feat: :tada: Inicio del Proyecto "DocumentacionPHP"
    Se crea los documentos de documentacion.php y style.css donde se forma una pagina web que da inicio a este proyecto, mostrando los puntos clavess del mismo (diseño del html (incrustado en php) y css)

---
+++ Documentacion.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>DocumentacionPHP</title>
+</head>
+<body>
+    <section class="visionBonita;3">
+    <div class="mars"></div>
+    <img src="img/space.png" class="logo-404"/><br><br><br><br><br>
+    <img src="https://assets.codepen.io/1538474/meteor.svg" class="meteor" />
+    <p class="title">Bienvenido a mi documentacion :3</p>
+    <p class="subtitle">
+    En este proyecto, nos enfocaremos en crear una página de documentación 
+    de PHP utilizando Git y GitHub. La documentación será generada y actualizada 
+    mediante commits en el repositorio de GitHub.
+    </p>
+    <div class="boton">
+        <a class="btn-back" href="#">Mira Los Commits</a>
+    </div>
+    <img src="https://assets.codepen.io/1538474/astronaut.svg" class="astronaut" />
+    <img src="https://assets.codepen.io/1538474/spaceship.svg" class="spaceship" />
+</body>
+</html>

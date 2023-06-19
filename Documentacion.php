1ede42e84af070725682c3b0b89c16e1f65d78dd
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-06-19 17:10:28 -0500

    feat: :sparkles: 1.7 Estructuras de control
    En esta seccion hablaremos sobre las estructuras de control, en la cual definiremos los diferentes tipos de estructuras (condicionales y repetitivos) ademas damos definicion y ejemplo de cada uno

--- Documentacion.php
+++ Documentacion.php
@@ -1,39 +1,150 @@
 <?php
 
-    print_r("1.6 Isset() y Empty()");
+    print_r("1.7 Estructuras de control");
 
     /*isset() y empty() son dos funciones en PHP utilizadas para verificar el estado de una variable.*/
 
 
-    print_r("isset()");
+    print_r("Estructura condicional");
 
-    /*La función isset() se utiliza para verificar si una variable está definida y tiene un valor asignado.
-    Retorna true si la variable existe y tiene un valor distinto de null.
-    Retorna false si la variable no existe o tiene un valor igual a null.*/
+    echo "if-else";
 
-    /*ejemplo*/
-
-    $nombre = "Juan";
-    var_dump(isset($nombre)); // bool (true)
-    
-    $TI = null;
-    var_dump(isset($TI)); // bool (false)
-    
-
-    print_r("empty()");
-
-    /*La función empty() se utiliza para verificar si una variable está vacía.
-    Retorna true si la variable no existe, tiene un valor igual a null, una cadena vacía, cero (0), un array vacío o un objeto sin propiedades.
-    Retorna false si la variable tiene algún valor asignado.*/
+    /*Permite ejecutar un bloque de código si se cumple una condición, y otro bloque 
+    de código si no se cumple la condición.*/
 
     /*ejemplo*/
 
-    $nombre = "Juan";
-    var_dump(empty($nombre)); // bool (false)
+    $edad = 25;
+    $es_estudiante = true;
+
+    if ($edad >= 18) {
+        echo "Eres mayor de edad.";
+
+        if ($es_estudiante) {
+            echo " Además, eres un estudiante.";
+        } else {
+            echo " No eres un estudiante.";
+        }
+    } else {
+        echo "Eres menor de edad.";
+    }
     
-    $edad = 0;
-    var_dump(empty($edad)); // bool (true)
-     
+    echo "switch-case";
+
+    /*Permite evaluar múltiples casos y ejecutar diferentes bloques de código 
+    según el caso que se cumpla.*/
+
+    /*ejemplo*/
+
+    $opcion = "B";
+
+    switch ($opcion) {
+        case "A":
+            echo "Seleccionaste la opción A";
+            break;
+        case "B":
+            echo "Seleccionaste la opción B";
+            break;
+        case "C":
+            echo "Seleccionaste la opción C";
+            break;
+        case "D":
+            echo "Seleccionaste la opción D";
+            break;
+        default:
+            echo "Opción inválida";
+            break;
+    }
+
+    echo "match";
+
+    /*La estructura "match" en PHP, introducida en la versión 8.0, es una alternativa 
+    a la estructura "switch-case" para realizar comparaciones más concisas y expresivas. 
+    Proporciona una sintaxis más simple y clara para realizar comparaciones de valores 
+    y ejecutar bloques de código en función del resultado.*/
+
+    /*ejemplo*/
+
+    $color = "rojo";
+
+    $match = match($color) {
+        'rojo' => "es color es $color",
+        'azul' => "es color es $color",
+        'verde' => "es color es $color",
+        'amarillo' => "es color es $color",
+        'dorado' => "es color es $color",
+        'NULL', 'null' => "no hay ninguno",
+        default => '',
+    };
+
+    var_dump($match);
+
+    print_r("Estructura repetición");
+
+    echo "while";
+
+    /*Permite ejecutar repetidamente un bloque de código mientras se 
+    cumpla una condición.*/
+
+    /*ejemplo*/
+
+    $numero = 1;
+    $suma = 0;
+
+    while ($numero <= 10) {
+        $suma += $numero;
+        $numero++;
+    }
+
+    echo "La suma de los números del 1 al 10 es: " . $suma;
+
+    echo "do-while";
+
+    /*Similar a la estructura while, pero garantiza que el bloque de 
+    código se ejecute al menos una vez antes de verificar la condición.*/
+
+    /*ejemplo*/
+
+    $intentos_maximos = 3;
+    $intentos = 0;
+    $contrasena_correcta = "secreto";
+    $contrasena_ingresada = "";
+
+    do {
+        $intentos++;
+        
+        echo "Introduce la contraseña: ";
+        $contrasena_ingresada = readline();
+        
+        if ($contrasena_ingresada == $contrasena_correcta) {
+            echo "¡Contraseña correcta! Acceso concedido.";
+            break;
+        } else {
+            echo "Contraseña incorrecta. Intento $intentos de $intentos_maximos.";
+        }
+    } while ($intentos < $intentos_maximos);
+
+    if ($intentos == $intentos_maximos) {
+        echo "Has excedido el número máximo de intentos. Acceso denegado.";
+    }
+
+    echo "for";
+
+    /*Permite ejecutar un bloque de código un número específico de veces.*/
+
+    /*ejemplo*/
+
+    $base = 2;
+    $potencia = 10;
+    $resultado = 1;
+
+    for ($i = 1; $i <= $potencia; $i++) {
+        $resultado *= $base;
+    }
+
+    echo "El resultado de elevar $base a la potencia $potencia es: $resultado";
+
+
 ?>
 
 <!DOCTYPE html>
